ad33638d73496269c11f799276e1be6fe334a616
samin_batra29 <saminbatra1@gmail.com>
2022-04-02 00:44:59 +0530

    Blog starter.

--- upgraded-blog/startbootstrap-clean-blog-gh-pages/main.py
+++ upgraded-blog/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
-
+from post import Post
 
 app = Flask(__name__)
+p = Post()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    posts = p.get_all()
+    return render_template("index.html", posts = posts)
 
 
 @app.route("/about")
@@ -18,6 +20,14 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
+@app.route("/posts/<index>")
+def get_posts(index):
+    b = p.get_blog_post(int(index))
+    print(b)
+    if b is not None:
+        return render_template("post.html", post=b)
+    return render_template("post.html", post={"title": "", "body": ""})
 
 if __name__=='__main__':
     app.run(debug=True)
