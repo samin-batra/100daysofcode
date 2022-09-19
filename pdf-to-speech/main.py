6c423d949bc5933dd12e26e5f2ed0bc1476691f3
samin_batra29 <saminbatra1@gmail.com>
2022-09-19 16:50:20 +0800

    Added more data about projects.

--- pdf-to-speech/main.py
+++ pdf-to-speech/main.py
@@ -7,6 +7,7 @@ pdfFileObj = open('Pride-and-Prejudice.pdf', 'rb')
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
+
 # printing number of pages in pdf file
 print(pdfReader.numPages)
 
